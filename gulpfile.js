8747bd01f2029ccca287c85ba4ac8b9efcdd465e
Stefan Walther <swr@qlik.com>
2015-08-24 00:54:34 +0200

    Add build system

--- gulpfile.js
+++ gulpfile.js
@@ -6,4 +6,17 @@ var path = require('path');
 var userConfig = senseGo.loadYml( path.join(__dirname, 'sense-go.yml'));
 senseGo.init( gulp, userConfig,  function (  ) {
 
+	gulp.task( 'build', gulp.series(
+		'clean:tmp',
+		'copy:toTmp',
+		'import',
+		'less:each',
+		'replace:tmp',
+		'jsonlint:tmp',
+		'htmlmin:tmp',
+		'clean:buildRelease',
+		'copy:tmpToRelease',
+		'clean:tmp'
+	) );
+
 });
