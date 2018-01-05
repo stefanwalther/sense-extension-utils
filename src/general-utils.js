4da891e25d9fe2ee197d9136d396fcc19e2be30f
Stefan Walther <stefanwalther@users.noreply.github.com>
2018-01-05 23:06:59 +0100

    style: new styling

--- src/general-utils.js
+++ src/general-utils.js
@@ -1,128 +1,128 @@
 /*global define*/
-define( [
-	'qlik',
-	'jquery',
-	'underscore'
-], function ( qlik, $, _ ) {
-	'use strict';
+define([
+  'qlik',
+  'jquery',
+  'underscore'
+], function (qlik, $, _) {
+  'use strict';
 
-	var $injector = angular.injector( ['ng'] );
-	var $q = $injector.get( "$q" );
-	var $http = $injector.get( "$http" );
+  var $injector = angular.injector(['ng']);
+  var $q = $injector.get("$q");
+  var $http = $injector.get("$http");
 
-	/**
-	 * Add as style link to the document's header
-	 *
-	 * @param {string} `linkUrl` - The Url to the CSS file.
-	 * @param {string?}  `id` - If an id is passed, it will be checked if this style link has already been added.
-	 * If this is the case, it will not be added again.
-	 *
-	 * @api public
-	 */
-	function addStyleLinkToHeader ( linkUrl, id ) {
-		if ( id && !_.isEmpty( id ) ) {
-			if ( !$( '#' + id ).length ) {
-				var $styleLink = $( document.createElement( 'link' ) );
-				$styleLink.attr( 'rel', 'stylesheet' );
-				$styleLink.attr( 'type', 'text/css' );
-				$styleLink.attr( 'href', linkUrl );
-				if ( id && !_.isEmpty( id ) ) {
-					$styleLink.attr( 'id', id );
-				}
-				$( 'head' ).append( $styleLink );
-			}
-		}
-	}
+  /**
+   * Add as style link to the document's header
+   *
+   * @param {string} `linkUrl` - The Url to the CSS file.
+   * @param {string?}  `id` - If an id is passed, it will be checked if this style link has already been added.
+   * If this is the case, it will not be added again.
+   *
+   * @api public
+   */
+  function addStyleLinkToHeader(linkUrl, id) {
+    if (id && !_.isEmpty(id)) {
+      if (!$('#' + id).length) {
+        var $styleLink = $(document.createElement('link'));
+        $styleLink.attr('rel', 'stylesheet');
+        $styleLink.attr('type', 'text/css');
+        $styleLink.attr('href', linkUrl);
+        if (id && !_.isEmpty(id)) {
+          $styleLink.attr('id', id);
+        }
+        $('head').append($styleLink);
+      }
+    }
+  }
 
-	/**
-	 * Add a style to the document's header.
-	 * @param cssContent {String} CSS content to be added to the header
-	 * @param id {String} If id is passed, addStyleToHeader will check if there has already been added a style with the given id, if yes, the css content will not be added to the header again
-	 */
-	function addStyleToHeader ( cssContent, id ) {
-		if ( id && typeof id === 'string' ) {
-			if ( !$( '#' + id ).length ) {
-				$( "<style>" )
-					.attr( 'id', id )
-					.html( cssContent ).appendTo( "head" );
-			}
-		} else {
-			$( "<style>" ).html( cssContent ).appendTo( "head" );
-		}
-	}
+  /**
+   * Add a style to the document's header.
+   * @param cssContent {String} CSS content to be added to the header
+   * @param id {String} If id is passed, addStyleToHeader will check if there has already been added a style with the given id, if yes, the css content will not be added to the header again
+   */
+  function addStyleToHeader(cssContent, id) {
+    if (id && typeof id === 'string') {
+      if (!$('#' + id).length) {
+        $("<style>")
+          .attr('id', id)
+          .html(cssContent).appendTo("head");
+      }
+    } else {
+      $("<style>").html(cssContent).appendTo("head");
+    }
+  }
 
-	function getBasePath () {
-		var prefix = window.location.pathname.substr( 0, window.location.pathname.toLowerCase().lastIndexOf( "/sense" ) + 1 );
-		var url = window.location.href;
-		url = url.split( "/" );
-		return url[0] + "//" + url[2] + ( ( prefix[prefix.length - 1] === "/" ) ? prefix.substr( 0, prefix.length - 1 ) : prefix );
-	}
+  function getBasePath() {
+    var prefix = window.location.pathname.substr(0, window.location.pathname.toLowerCase().lastIndexOf("/sense") + 1);
+    var url = window.location.href;
+    url = url.split("/");
+    return url[0] + "//" + url[2] + ((prefix[prefix.length - 1] === "/") ? prefix.substr(0, prefix.length - 1) : prefix);
+  }
 
-	function getExtensionInfo ( extensionUniqueName ) {
-		var defer = $q.defer();
+  function getExtensionInfo(extensionUniqueName) {
+    var defer = $q.defer();
 
-		var url = getBasePath() + '/extensions/' + extensionUniqueName + '/' + extensionUniqueName + '.qext';
-		$http.get( url )
-			.then( function ( response ) {
-				defer.resolve( response.data );
-			} ).catch( function ( err ) {
-			defer.reject( err );
-		} );
+    var url = getBasePath() + '/extensions/' + extensionUniqueName + '/' + extensionUniqueName + '.qext';
+    $http.get(url)
+      .then(function (response) {
+        defer.resolve(response.data);
+      }).catch(function (err) {
+      defer.reject(err);
+    });
 
-		return defer.promise;
-	}
+    return defer.promise;
+  }
 
-	function getExtensionPath ( extensionUniqueName ) {
-		return window.location.pathname.substr( 0, window.location.pathname.toLowerCase().lastIndexOf( "/sense" ) + 1 ) + 'extensions/' + extensionUniqueName;
-	}
+  function getExtensionPath(extensionUniqueName) {
+    return window.location.pathname.substr(0, window.location.pathname.toLowerCase().lastIndexOf("/sense") + 1) + 'extensions/' + extensionUniqueName;
+  }
 
-	function getProductVersion () {
-		var defer = $q.defer();
-		var global = qlik.getGlobal( {} );
+  function getProductVersion() {
+    var defer = $q.defer();
+    var global = qlik.getGlobal({});
 
-		global.getProductVersion( function ( reply ) {
-			var v = reply.qReturn;
-			var lastDot = xIndexOf( v, '.', 2 );
-			var rest = v.substr( lastDot + 1 );
-			var chars = rest.split();
-			var numDigitsAfterRest = 0;
-			for ( var i = 0; i < chars.length; i++ ) {
-				if ( !_.isNumber( chars[i] ) ) {
-					numDigitsAfterRest = i + 1;
-					break;
-				}
-			}
-			defer.resolve( v.substr( 0, lastDot + 1 + numDigitsAfterRest ) );
-		} );
+    global.getProductVersion(function (reply) {
+      var v = reply.qReturn;
+      var lastDot = xIndexOf(v, '.', 2);
+      var rest = v.substr(lastDot + 1);
+      var chars = rest.split();
+      var numDigitsAfterRest = 0;
+      for (var i = 0; i < chars.length; i++) {
+        if (!_.isNumber(chars[i])) {
+          numDigitsAfterRest = i + 1;
+          break;
+        }
+      }
+      defer.resolve(v.substr(0, lastDot + 1 + numDigitsAfterRest));
+    });
 
-		return defer.promise;
-	}
+    return defer.promise;
+  }
 
-	if ( typeof String.prototype.startsWith != 'function' ) {
-		String.prototype.startsWith = function ( str ) {
-			if ( str == null ) {
-				return false;
-			}
-			var i = str.length;
-			if ( this.length < i ) {
-				return false;
-			}
-			for ( --i; (i >= 0) && (this[i] === str[i]); --i ) {
+  if (typeof String.prototype.startsWith != 'function') {
+    String.prototype.startsWith = function (str) {
+      if (str == null) {
+        return false;
+      }
+      var i = str.length;
+      if (this.length < i) {
+        return false;
+      }
+      for (--i; (i >= 0) && (this[i] === str[i]); --i) {
 
-			}
-			return i < 0;
-		}
-	}
+      }
+      return i < 0;
+    }
+  }
 
-	/**
-	 * @api public
-	 */
-	return {
-		addStyleToHeader: addStyleToHeader,
-		addStyleLinkToHeader: addStyleLinkToHeader,
-		getExtensionInfo: getExtensionInfo,
-		getExtensionPath: getExtensionPath,
-		getProductVersion: getProductVersion,
-		getBasePath: getBasePath
-	}
-} );
+  /**
+   * @api public
+   */
+  return {
+    addStyleToHeader: addStyleToHeader,
+    addStyleLinkToHeader: addStyleLinkToHeader,
+    getExtensionInfo: getExtensionInfo,
+    getExtensionPath: getExtensionPath,
+    getProductVersion: getProductVersion,
+    getBasePath: getBasePath
+  }
+});
--- src/pp-helper.js
+++ src/pp-helper.js
@@ -1,120 +1,120 @@
 /*global window,define*/
-define( [
-	'angular',
-	'underscore',
-	'qlik'
-], function ( angular, _, qlik ) {
+define([
+  'angular',
+  'underscore',
+  'qlik'
+], function (angular, _, qlik) {
 
-	var $injector = angular.injector( ['ng'] );
-	var $q = $injector.get( "$q" );
+  var $injector = angular.injector(['ng']);
+  var $q = $injector.get("$q");
 
-	var app = qlik.currApp();
+  var app = qlik.currApp();
 
-	/**
-	 * Collection of helpers to retrieve data which can be easily used in the property panel
-	 * of visualization extensions.
-	 */
-	var PPHelper = function () {
+  /**
+   * Collection of helpers to retrieve data which can be easily used in the property panel
+   * of visualization extensions.
+   */
+  var PPHelper = function () {
 
-		// Todo: probably easier if we added some sorting for the apps
-		this.getAppList = function () {
-			var defer = $q.defer();
+    // Todo: probably easier if we added some sorting for the apps
+    this.getAppList = function () {
+      var defer = $q.defer();
 
-			qlik.getGlobal().getAppList( function ( items ) {
-				defer.resolve( items.map( function ( item ) {
-						return {
-							value: item.qDocId,
-							label: item.qTitle
-						}
-					} )
-				);
-			} );
+      qlik.getGlobal().getAppList(function (items) {
+        defer.resolve(items.map(function (item) {
+            return {
+              value: item.qDocId,
+              label: item.qTitle
+            }
+          })
+        );
+      });
 
-			return defer.promise;
-		};
+      return defer.promise;
+    };
 
-		// Todo: Add sorting for the list of bookmarks
-		this.getBookmarkList = function () {
-			var defer = $q.defer();
+    // Todo: Add sorting for the list of bookmarks
+    this.getBookmarkList = function () {
+      var defer = $q.defer();
 
-			app.getList( 'BookmarkList', function ( items ) {
-				defer.resolve( items.qBookmarkList.qItems.map( function ( item ) {
-					return {
-						value: item.qInfo.qId,
-						label: item.qData.title
-					};
-				} ) );
-			} );
-			return defer.promise;
-		};
+      app.getList('BookmarkList', function (items) {
+        defer.resolve(items.qBookmarkList.qItems.map(function (item) {
+          return {
+            value: item.qInfo.qId,
+            label: item.qData.title
+          };
+        }));
+      });
+      return defer.promise;
+    };
 
-		this.getFieldList = function () {
-			var defer = $q.defer();
+    this.getFieldList = function () {
+      var defer = $q.defer();
 
-			app.getList( "FieldList", function ( items ) {
-				// console.log( 'fieldList', items );
+      app.getList("FieldList", function (items) {
+        // console.log( 'fieldList', items );
 
-				defer.resolve( items.qFieldList.qItems.map( function ( item ) {
-					return {
-						value: item.qName,
-						label: item.qName
-					};
-				} ) );
-			} );
+        defer.resolve(items.qFieldList.qItems.map(function (item) {
+          return {
+            value: item.qName,
+            label: item.qName
+          };
+        }));
+      });
 
-			return defer.promise;
-		};
+      return defer.promise;
+    };
 
-		//Todo: Check if sorting really works by qData.rank
-		this.getSheetList = function () {
+    //Todo: Check if sorting really works by qData.rank
+    this.getSheetList = function () {
 
-			var defer = $q.defer();
+      var defer = $q.defer();
 
-			app.getAppObjectList( function ( data ) {
-				var sheets = [];
-				var sortedData = _.sortBy( data.qAppObjectList.qItems, function ( item ) {
-					return item.qData.rank;
-				} );
-				_.each( sortedData, function ( item ) {
-					sheets.push( {
-						value: item.qInfo.qId,
-						label: item.qMeta.title
-					} );
-				} );
-				return defer.resolve( sheets );
-			} );
+      app.getAppObjectList(function (data) {
+        var sheets = [];
+        var sortedData = _.sortBy(data.qAppObjectList.qItems, function (item) {
+          return item.qData.rank;
+        });
+        _.each(sortedData, function (item) {
+          sheets.push({
+            value: item.qInfo.qId,
+            label: item.qMeta.title
+          });
+        });
+        return defer.resolve(sheets);
+      });
 
-			return defer.promise;
-		};
+      return defer.promise;
+    };
 
-		this.getStoryList = function () {
+    this.getStoryList = function () {
 
-			var defer = $q.defer();
+      var defer = $q.defer();
 
-			app.getList( 'story', function ( data ) {
-				var stories = [];
-				if ( data && data.qAppObjectList && data.qAppObjectList.qItems ) {
-					data.qAppObjectList.qItems.forEach( function ( item ) {
-						stories.push( {
-							value: item.qInfo.qId,
-							label: item.qMeta.title
-						} );
-					} )
-				}
-				return defer.resolve( _.sortBy( stories, function ( item ) {
-					return item.label;
-				} ) );
+      app.getList('story', function (data) {
+        var stories = [];
+        if (data && data.qAppObjectList && data.qAppObjectList.qItems) {
+          data.qAppObjectList.qItems.forEach(function (item) {
+            stories.push({
+              value: item.qInfo.qId,
+              label: item.qMeta.title
+            });
+          })
+        }
+        return defer.resolve(_.sortBy(stories, function (item) {
+          return item.label;
+        }));
 
-			} );
+      });
 
-			return defer.promise;
+      return defer.promise;
 
-		}
-	};
+    }
+  };
 
-	// ****************************************************************************************
-	// Return Value
-	// ****************************************************************************************
-	return new PPHelper();
+  // ****************************************************************************************
+  // Return Value
+  // ****************************************************************************************
+  return new PPHelper();
 
-} );
+});
