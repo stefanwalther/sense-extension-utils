29697329df312f54e48f2c177ef80461cf127124
Stefan Walther <swr@qlik.com>
2018-01-19 23:28:01 -0500

    test(variable-utils): add basic tests

---
+++ test/unit/variable-utils.spec.js
@@ -0,0 +1,19 @@
+/* global describe, it */
+'use strict';
+
+define(['chai', './../../src/variable-utils'], function (chai, variableUtils) {
+  var expect = chai.expect;
+  describe('general-utils', function () {
+    it('should expose functions', function () {
+      expect(variableUtils).to.have.a.property('updateEngineVars').to.be.a('function');
+      expect(variableUtils).to.have.a.property('ensureEngineVarExists').to.be.a('function');
+      expect(variableUtils).to.have.a.property('createEngineSessionVar').to.be.a('function');
+      expect(variableUtils).to.have.a.property('engineVarExists').to.be.a('function');
+      expect(variableUtils).to.have.a.property('getEngineVarListValues').to.be.a('function');
+      expect(variableUtils).to.have.a.property('getEngineVarValue').to.be.a('function');
+    });
+    it('should not expose the following functions', function() {
+      expect(variableUtils).to.not.have.a.property('does-not-exist');
+    })
+  });
+});
